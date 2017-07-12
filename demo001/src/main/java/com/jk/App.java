aba556fc7930271d1ab98d8cd6a6b3dc75326b5e
jiekekeji <jieke_rose@foxmail.com>
2017-07-12 10:34:14 +0800

    spring4-整理归纳

---
+++ demo001/src/main/java/com/jk/App.java
@@ -0,0 +1,25 @@
+package com.jk;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		// 1. 创建 Spring 的 IOC 容器: 从类路径加载xml配置文件
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("bean.xml");
+
+		// 2.从 IOC 容器中获取 bean 的实例:以id的方式
+		// User user = (User) ctx.getBean("user");
+
+		// 2.从 IOC 容器中获取 bean 的实例:以类型的方式获取
+		User user = (User) ctx.getBean(User.class);
+
+		System.out.println(user);
+
+	}
+}
